63c865aaedff0017c94852cf1c3368dc40951f6c
swanhtetmac <swanhtet102002@gmail.com>
2023-12-12 17:33:13 +0100

    SOLID principle in python

--- PYTHON/alogrithm.py
+++ PYTHON/alogrithm.py
@@ -1,9 +1,11 @@
 import random
 
+
 class Sorter:
     def sort(self,data):
         raise NotImplementedError
-    
+
+
 class BubbleSort(Sorter):
     def sort(self, data):
         length = len(data)
@@ -12,9 +14,22 @@ class BubbleSort(Sorter):
                 if  data[j ] > data[j+1]:
                     data[j], data[j+1] = data[j + 1],data[j]
 
+
+class SelectionSort(Sorter):
+    def sort(self, data):
+        length = len(data)
+        for step in range(0, length):
+            min_index = step
+            for i in range(step + 1, length):
+                if data[i] < data[min_index]:
+                    min_index = i
+            data[min_index], data[i] = data[i], data[min_index]
+
+
 class PrintingSort:
     def PrintSort(self, data):
         print(data)
+
 
 class Alogrithm:
     def __init__(self, sorter, printer):
@@ -25,11 +40,13 @@ class Alogrithm:
         self.sorter.sort(data)
         self.printer.PrintSort(data)
 
+
 if __name__ == '__main__':
     random.seed()
     data_sort = [random.randint(0,100) for _ in range(10)]
     print(data_sort)
-    sorter = BubbleSort()
+    sorter = SelectionSort()
     printer = PrintingSort()
+
     alogrithm_object = Alogrithm(sorter=sorter, printer=printer)
     alogrithm_object.run(data_sort)
